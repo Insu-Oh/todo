c2cb229dc272514bac63132f2f6c74b36d06d1a1
insu <ohinsu134@gmail.com>
2021-03-03 15:09:56 +0900

    greeting a bit

--- greeting.js
+++ greeting.js
@@ -1,1 +1,27 @@
-const form = document.querySelector('.form');
+const form = document.querySelector('.form'),
+  input = form.querySelector('.inputText');
+
+const NAME = "userName";
+
+function loadName() {
+  const userName =  localStorage.getItem(NAME);
+  if(userName === null) {
+    displayNameAdd();
+  } else {
+    displayName();
+  }
+}
+
+function displayNameAdd() {
+
+}
+
+function displayName() {
+
+}
+
+function init() {
+  loadName();
+}
+
+init();
